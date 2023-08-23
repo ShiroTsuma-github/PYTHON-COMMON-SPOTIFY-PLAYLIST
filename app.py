23ddbd31a8f692c44dd0d74937883ef542b56696
ShiroTsuma-github <spam.shiro@gmail.com>
2023-08-23 14:33:17 +0200

    finally working session of spotify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from typing import Literal
+import pickle
 from flask import Flask, render_template, url_for, redirect, session, request
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -28,27 +28,22 @@ class SpotifyPlus():
             client_secret=api_sec,
             redirect_uri=self.redirect,
             scope=self.scope)
-        self.__token = None
-        self.__atoken = None
-        self.__spotify = None
+        self.accessToken = None
+        self.spotify = None
 
     def getAuth(self) -> SpotifyOAuth:
         return self.OAuth
 
     def getSpotify(self):
-        if self.__spotify is not None:
-            return self.__spotify
-        raise ValueError("Token not set")
+        if self.accessToken is None:
+            raise ValueError("Token not set")
+        return spotipy.Spotify(auth=self.accessToken)
 
     def isAccess(self):
-        if self.__atoken is None:
-            return False
-        return True
+        return self.accessToken is not None
 
     def setAccessToken(self, code):
-        self.__token = self.OAuth.get_access_token(code)
-        self.__atoken = self.__token['access_token']
-        self.__spotify = spotipy.Spotify(self.__atoken)
+        self.accessToken = self.OAuth.get_access_token(code)['access_token']
 
     def getAuthUrl(self) -> str:
         return self.OAuth.get_authorize_url()
@@ -56,9 +51,9 @@ class SpotifyPlus():
 
 @app.route("/artists")
 def get_artists():
-    sp_plus = session.get('sp_plus')
+    sp_plus = pickle.loads(session['sp_plus'])
     if not sp_plus.isAccess():
-        return redirect(url_for('.login'))
+        return redirect(url_for('login'))
     sp = sp_plus.getSpotify()
     res = sp.current_user_followed_artists()
     res = [res.get('artists').get('items')[i].get('name')
@@ -69,20 +64,16 @@ def get_artists():
 
 @app.route('/login')
 def login():
-    sp_plus = session.get('sp_plus')
-    if sp_plus is None:
-        return redirect(url_for('.main_page'))
+    sp_plus = pickle.loads(session['sp_plus'])
     return redirect(sp_plus.getAuthUrl())
 
 
 @app.route('/redirect')
 def redirectPage():
-    sp_plus = session.get('sp_plus')
-    if sp_plus is None:
-        return redirect(url_for('.main_page'))
-    sp_plus.setAccessToken(request.args['code'])
-
-    return redirect(url_for('.main_page'))
+    sp_plus = pickle.loads(session['sp_plus'])
+    sp_plus.setAccessToken(request.args.get('code'))
+    session['sp_plus'] = pickle.dumps(sp_plus)
+    return redirect(url_for('get_profile'))
 
 
 @app.route("/clear")
@@ -94,22 +85,26 @@ def clear_session():
 
 @app.route("/me")
 def get_profile():
-    sp_plus = session.get('sp_plus')
+    sp_plus = pickle.loads(session['sp_plus'])
     if not sp_plus.isAccess():
-        return redirect(url_for('.main_page'))
+        return redirect(url_for('main_page'))
     sp = sp_plus.getSpotify()
     res = sp.current_user()
-    session['me'] = res
+    nick = res.get('display_name')
+    img = None if len(res.get('images')) == 0 else res.get('images')[0].get('url')
+    id = res.get('id')
+    email = res.get('email')
+    session['me'] = [nick, img, id, email]
     return redirect(url_for('.main_page'))
 
 
 @app.route("/")
 def main_page():
-    global sp_plus
-    sp_plus = SpotifyPlus(
-        "user-follow-read,playlist-modify-public",
-        url_for('.redirectPage', _external=True))
-    session['sp_plus'] = sp_plus
-    messages = session.get('res')
-    me = session.get('me')
+    if 'sp_plus' not in session:
+        sp_plus = SpotifyPlus(
+            "user-follow-read,playlist-modify-public,user-follow-modify,user-library-modify,user-library-read,user-read-email,user-read-private",
+            url_for('redirectPage', _external=True))
+        session['sp_plus'] = pickle.dumps(sp_plus)
+    messages = session.get('res', [])
+    me = session.get('me', None)
     return render_template("mainpage.html", messages=messages, me=me)
